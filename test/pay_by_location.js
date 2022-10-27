9eed2ba585a36218974c70d029f371aaac046bc1
natty <natnaelmasresha@gmail.com>
2022-10-27 20:59:32 +0300

    use proper contract name

--- test/pay_by_location.js
+++ test/pay_by_location.js
@@ -5,7 +5,7 @@ const payByLocation = artifacts.require("PayByLocation");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("payTest", function (/* accounts */) {
+contract("PayByLocation", function (/* accounts */) {
   it("should assert true", async function () {
     await payByLocation.deployed();
     return assert.isTrue(true);
