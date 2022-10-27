9aa5ae30e356b6b257e7d0e621bdd8d876f78b30
natty <natnaelmasresha@gmail.com>
2022-10-27 16:20:10 +0300

    add test

---
+++ test/pay_by_location.js
@@ -0,0 +1,13 @@
+const PayByLocation = artifacts.require("PayByLocation");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("PayByLocation", function (accounts) {
+  it("should assert true", async function () {
+    await PayByLocation.deployed();
+    return assert.isTrue(true);
+  });
+});
