700bada908ad5b4d99d4e328a7276bf6a3d419f4
natty <natnaelmasresha@gmail.com>
2022-10-28 21:48:27 +0300

    use proper naming for migration

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,4 +1,4 @@
 const PayByLocation = artifacts.require("PayByLocation");
-module.exports = function(deployer) {
+module.exports = function(deployer, accounts) {
   deployer.deploy(PayByLocation);
 };
